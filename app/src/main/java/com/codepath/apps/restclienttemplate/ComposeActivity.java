f37a0ce242e9488f435c0af3d4f488984449518d
samliu000 <44206874+samliu000@users.noreply.github.com>
2020-11-13 21:50:22 -0800

    Added demo and finished text count

--- app/src/main/java/com/codepath/apps/restclienttemplate/ComposeActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/ComposeActivity.java
@@ -3,16 +3,22 @@ package com.codepath.apps.restclienttemplate;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Parcel;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.codepath.apps.restclienttemplate.models.Tweet;
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.parceler.Parcels;
 
@@ -21,9 +27,14 @@ import okhttp3.Headers;
 public class ComposeActivity extends AppCompatActivity {
 
     public static final String TAG = "ComposeActivity";
+    public static final  int MAX_TWEET_LENGTH = 140;
+
     EditText etCompose;
     Button btnTweet;
+    TextView charCount;
+
     TwitterClient client;
+    private TextWatcher mTextEditorWatcher;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,33 +45,53 @@ public class ComposeActivity extends AppCompatActivity {
 
         etCompose = findViewById(R.id.etCompose);
         btnTweet = findViewById(R.id.btnTweet);
+        charCount = findViewById(R.id.charCount);
+
+        mTextEditorWatcher = new TextWatcher() {
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                //This sets a textview to the current length
+                charCount.setText(String.valueOf(s.length()) + "/280");
+                if(s.length() > 280) {
+                    charCount.setTextColor(Color.RED);
+                }
+            }
+
+            public void afterTextChanged(Editable s) {
+            }
+        };
+
+        etCompose.addTextChangedListener(mTextEditorWatcher);
 
         // Set click listener on button
         btnTweet.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(View view) {
                 String tweetContent = etCompose.getText().toString();
-                if(tweetContent.isEmpty()) {
-                    Toast.makeText(ComposeActivity.this, "Sorry, your tweet cannot be empty", Toast.LENGTH_LONG).show();
+                if(tweetContent.isEmpty()){
+                    Toast.makeText(ComposeActivity.this, "Sorry your tweet cannot be empty", Toast.LENGTH_SHORT).show();
                     return;
                 }
-                if(tweetContent.length() > 140) {
-                    Toast.makeText(ComposeActivity.this, "Sorry, your tweet is too long", Toast.LENGTH_LONG).show();
+                if(tweetContent.length() > MAX_TWEET_LENGTH){
+                    Toast.makeText(ComposeActivity.this, "Sorry your tweet is too long", Toast.LENGTH_SHORT).show();
                     return;
                 }
-                Toast.makeText(ComposeActivity.this, tweetContent, Toast.LENGTH_LONG).show();
-
-                //Make an API call to Twitter to publish the tweet
+                Toast.makeText(ComposeActivity.this, tweetContent, Toast.LENGTH_SHORT).show();
+                // Make an API call to Twitter to publish the tweet
                 client.publishTweet(tweetContent, new JsonHttpResponseHandler() {
                     @Override
                     public void onSuccess(int statusCode, Headers headers, JSON json) {
-                        Log.i(TAG, "onSuccess to publish tweet");
+                        Log.e(TAG, "onSuccess to publish tweet");
                         try {
                             Tweet tweet = Tweet.fromJson(json.jsonObject);
-                            Log.i(TAG, "Published Tweet says: " + tweet.body);
+                            Log.i(TAG, "Published tweet says: " + tweet);
                             Intent intent = new Intent();
                             intent.putExtra("tweet", Parcels.wrap(tweet));
+                            // Set result code and bundle up for response
                             setResult(RESULT_OK, intent);
+                            // closes the activity, pass data to parent
                             finish();
                         } catch (JSONException e) {
                             e.printStackTrace();
@@ -69,7 +100,7 @@ public class ComposeActivity extends AppCompatActivity {
 
                     @Override
                     public void onFailure(int statusCode, Headers headers, String response, Throwable throwable) {
-                        Log.e(TAG, "onFailure to publish Tweet", throwable);
+                        Log.e(TAG, "onFailure to publish tweet", throwable);
                     }
                 });
             }
